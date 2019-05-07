e1f3ed92c161836d7022956513393167811dc42a
thansoft <thansoft.kb@gmail.com>
2019-05-07 14:45:50 +0530

    category service added

--- FlaskApp.py
+++ FlaskApp.py
@@ -14,66 +14,68 @@ api = Api(app, version='1.0', title='POC API',
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://4xBEJzOgUQ:thansoft105@remotemysql.com/4xBEJzOgUQ"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-orm = SQLAlchemy(app)
-serializer = Marshmallow(app)
-
-
-# category master
-class CategoryMaster(orm.Model):
-    id = orm.Column(orm.Integer, primary_key=True)
-    category = orm.Column(orm.String(100), nullable=False)
-    createdDate = orm.Column(orm.DateTime, nullable=False)
-    createdBy = orm.Column(orm.Integer, nullable=False)
-
-
-class CategoryMasterSchema(serializer.ModelSchema):
-    class Meta:
-        model = CategoryMaster
-
-
-#  category master schema
-class CategorySchema(Schema):
-    id = ma_fields.Integer()
-    category = ma_fields.String()
-    createdDate = ma_fields.DateTime()
-    createdBy = ma_fields.Integer()
-
-
-# category_schema instance
-category_Schema = CategorySchema()
-categories_Schema = CategorySchema(many=True)
-
-categoryMasterSchema = CategoryMasterSchema()
-
-# category model
 category_ns = api.namespace('category', description='category in budget')
-category_model = api.model('category', {
-    'id': fields.Integer(description='category unique identifier'),
-    'category': fields.String(required=True, description='name of the category'),
-    'createdDate': fields.DateTime(dt_format='iso8601', description="category created date and time"),
-    'createdBy': fields.Integer(description='category created user')
-})
+
+# orm = SQLAlchemy(app)
+# serializer = Marshmallow(app)
 
 
-# category list operations
-@category_ns.route('')
-class CategoryList(Resource):
-
-    @category_ns.doc('list categories')
-    @category_ns.response(200, 'ok')
-    @category_ns.marshal_list_with(category_model)
-    def get(self):
-        return categories_Schema.dump(CategoryMaster.query.all()).data
-
-    @category_ns.doc('create category')
-    @category_ns.response(201, 'Created')
-    @category_ns.expect(category_model)
-    def post(self):
-        category = categoryMasterSchema.load(api.payload).data
-        orm.session.add(category)
-        orm.session.commit()
-        return "category created successfully", 201
-
+# # category master
+# class CategoryMaster(orm.Model):
+#     id = orm.Column(orm.Integer, primary_key=True)
+#     category = orm.Column(orm.String(100), nullable=False)
+#     createdDate = orm.Column(orm.DateTime, nullable=False)
+#     createdBy = orm.Column(orm.Integer, nullable=False)
+#
+#
+# class CategoryMasterSchema(serializer.ModelSchema):
+#     class Meta:
+#         model = CategoryMaster
+#
+#
+# #  category master schema
+# class CategorySchema(Schema):
+#     id = ma_fields.Integer()
+#     category = ma_fields.String()
+#     createdDate = ma_fields.DateTime()
+#     createdBy = ma_fields.Integer()
+#
+#
+# # category_schema instance
+# category_Schema = CategorySchema()
+# categories_Schema = CategorySchema(many=True)
+#
+# categoryMasterSchema = CategoryMasterSchema()
+#
+# # category model
+# category_ns = api.namespace('category', description='category in budget')
+# category_model = api.model('category', {
+#     'id': fields.Integer(description='category unique identifier'),
+#     'category': fields.String(required=True, description='name of the category'),
+#     'createdDate': fields.DateTime(dt_format='iso8601', description="category created date and time"),
+#     'createdBy': fields.Integer(description='category created user')
+# })
+#
+#
+# # category list operations
+# @category_ns.route('')
+# class CategoryList(Resource):
+#
+#     @category_ns.doc('list categories')
+#     @category_ns.response(200, 'ok')
+#     @category_ns.marshal_list_with(category_model)
+#     def get(self):
+#         return categories_Schema.dump(CategoryMaster.query.all()).data
+#
+#     @category_ns.doc('create category')
+#     @category_ns.response(201, 'Created')
+#     @category_ns.expect(category_model)
+#     def post(self):
+#         category = categoryMasterSchema.load(api.payload).data
+#         orm.session.add(category)
+#         orm.session.commit()
+#         return "category created successfully", 201
+#
 
 @category_ns.route('/<int:cat_id>')
 @category_ns.param('cat_id', 'category id')
@@ -90,5 +92,5 @@ class Category(Resource):
 
 
 if __name__ == "__main__":
-    orm.create_all()
-    app.run(debug=True, port=environ.get('PORT', 5000), host="0.0.0.0")
+    # orm.create_all()
+    app.run(debug=False, port=environ.get('PORT', 5000), host="0.0.0.0")
